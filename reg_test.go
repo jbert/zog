bbe72fbb4331abb2b82654a241cab6e5f454c621
John Berthels <john@berthels.co.uk>
2017-03-04 09:11:37 +0000

    use t.Errorf in tests

--- reg_test.go
+++ reg_test.go
@@ -11,17 +11,15 @@ func TestLoadSave8(t *testing.T) {
 	z := New(1024)
 	for _, r := range regs {
 		n := z.Read8(r)
-		log.Printf("Is %s initially zero", r)
 		if n != 0 {
-			t.Fail()
+			t.Errorf("%s is not initially zero", r)
 		}
 		nStore := byte(0x7f)
 		log.Printf("Store %d to %s", nStore, r)
 		z.Write8(r, nStore)
 		n = z.Read8(r)
-		log.Printf("Does %s now contain %d", r, nStore)
 		if n != nStore {
-			t.Fail()
+			t.Errorf("%s does not now contain", r, nStore)
 		}
 	}
 }
@@ -31,17 +29,16 @@ func TestLoadSave16(t *testing.T) {
 	z := New(1024)
 	for _, r := range regs {
 		nn := z.Read16(r)
-		log.Printf("Is %s initially zero", r)
 		if nn != 0 {
-			t.Fail()
+			t.Errorf("%s is not initially zero", r)
 		}
 		nnStore := uint16(0x1234)
 		log.Printf("Store %d to %s", nnStore, r)
 		z.Write16(r, nnStore)
 		nn = z.Read16(r)
-		log.Printf("Does %s now contain %d", r, nnStore)
 		if nn != nnStore {
 			t.Fail()
+			t.Errorf("%s does not now contain %d", r, nnStore)
 		}
 	}
 }
@@ -51,15 +48,13 @@ func TestHiLoLoadSaveAdHoc(t *testing.T) {
 	z := New(1024)
 	log.Printf("Write 0x1234 to BC")
 	z.Write16(BC, 0x1234)
-	log.Printf("Does B contain 0x12?")
 	n := z.Read8(B)
 	if n != 0x12 {
-		t.Fail()
+		t.Errorf("B doesn't contain 0x12 after BC load")
 	}
-	log.Printf("Does C contain 0x34?")
 	n = z.Read8(C)
 	if n != 0x34 {
-		t.Fail()
+		t.Errorf("C doesn't contain 0x34 after BC load")
 	}
 }
 
@@ -93,12 +88,10 @@ func TestDecodeLDBasic(t *testing.T) {
 		}
 		i, err := Decode(getNext)
 		if err != nil {
-			log.Printf("Failed: %s", err)
-			t.Fail()
+			t.Errorf("Failed: %s", err)
 		}
 		if i.String() != tc.str {
-			log.Printf("Wrong instruction: %s != %s", i, tc.str)
-			t.Fail()
+			t.Errorf("Wrong instruction: %s != %s", i, tc.str)
 		}
 		log.Printf("Decoded %s", i)
 	}
